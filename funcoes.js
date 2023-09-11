cdd5f5e6a0dc21f1c4e46746cfde4db9e981a204
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2023-09-11 18:48:03 -0300

    terceiro envio

--- funcoes.js
+++ funcoes.js
@@ -1,17 +1,9 @@
-const verificaNum = (numero) =>{
-      return !isNaN(parseFloat(numero)) && isFinite(numero);
-}
+
 const jurosSimples = (capital,juros,tempo) =>{
-      if(!verificaNum(capital)|| !verificaNum(juros)|| !verificaNum(tempo)){
-         throw new Error('Todos os valores devem ser numeros validos');
-      }
       return (capital*juros*tempo)/100;
 }
 
 const Montante = (capital,juros,tempo) =>{
-      if(!verificaNum(capital)|| !verificaNum(juros)|| !verificaNum(tempo)){
-         throw new Error('Todos os valores devem ser numeros validos');
-      }
       return capital+jurosSimples(capital,juros,tempo);
 
 }
--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 import express from 'express';
-import {jurosSimples,Montante} from './funcoes.js';
+import { jurosSimples, Montante } from './funcoes.js';
+
 const app = express();
 
-app.get('/juros-simples',(req,res)=>{
+app.get('/juros-simples', (req, res) => {
     const capital = parseFloat(req.query.capital);
     const taxaJuros = parseFloat(req.query.juros);
     const tempo = parseFloat(req.query.tempo);
-    if(isNaN(capital)|| isNaN(taxaJuros)|| isNaN(tempo)){
-        res.status(400).json({error: 'Os parametros tem que ser numeros validos' });
-    }else{
-        const juros = jurosSimples(capital,taxaJuros,tempo);
-        res.json({juros:juros});
-    }
-});
-app.get('/montante',(req,res)=>{
-    const capital = parseFloat(req.query.capital);
-    const taxaJuros = parseFloat(req.query.taxaJuros);
-    const tempo = parseFloat(req.query.tempo)
-     if(isNaN(capital)|| isNaN(taxaJuros)|| isNaN(tempo)){
-        res.status(400).json({error: 'Os parametros tem que ser numeros validos'});
-    }else{
-        const montante = Montante(capital,taxaJuros,tempo);
-        res.json({montante:montante});
+    if (isNaN(capital) || isNaN(taxaJuros) || isNaN(tempo)) {
+        res.status(400).json({ error: 'Os parâmetros devem ser números válidos' });
+    } else {
+        const juros = jurosSimples(capital, taxaJuros, tempo);
+        res.json({ juros: juros });
     }
 });
 
-app.listen(8080,()=>{
-    console.log("App estar rodando!");
+app.get('/montante', (req, res) => {
+    const capital = parseFloat(req.query.capital);
+    const taxaJuros = parseFloat(req.query.juros);
+    const tempo = parseFloat(req.query.tempo);
+    if (isNaN(capital) || isNaN(taxaJuros) || isNaN(tempo)) {
+        res.status(400).json({ error: 'Os parâmetros devem ser números válidos' });
+    } else {
+        const montante = Montante(capital, taxaJuros, tempo);
+        res.json({ montante: montante });
+    }
+});
+
+app.listen(8080, () => {
+    console.log("O aplicativo está rodando!");
 });
